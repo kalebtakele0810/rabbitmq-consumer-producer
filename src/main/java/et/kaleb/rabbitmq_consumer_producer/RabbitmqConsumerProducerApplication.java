c62d119708df4648c174d3b338c0dc7ce6f9f5b4
Kaleb Takele <kaleb.takele0810@gmail.com>
2024-10-02 21:43:17 +0400

    initial commit and proj skeleton

---
+++ src/main/java/et/kaleb/rabbitmq_consumer_producer/RabbitmqConsumerProducerApplication.java
@@ -0,0 +1,13 @@
+package et.kaleb.rabbitmq_consumer_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqConsumerProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqConsumerProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/et/kaleb/rabbitmq_consumer_producer/RabbitmqConsumerProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package et.kaleb.rabbitmq_consumer_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqConsumerProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
